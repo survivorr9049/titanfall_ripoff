bd8ce1c8f8bb6ad2d382047f925a844ce0f9b69c
survivorr <survivorowy@gmail.com>
2021-01-10 03:54:37 +0100

    wallrun improvements

--- Assets/Scripts/characterController.cs
+++ Assets/Scripts/characterController.cs
@@ -11,6 +11,7 @@ public class characterController : MonoBehaviour
     public Text text;
     public Transform camera;
     public Transform ground;
+    public Transform cameraParent;
 
     [Header("Basic Movement")]
     public float movementSpeed;
@@ -24,6 +25,7 @@ public class characterController : MonoBehaviour
     public float gravity;
 
     [Header("WallRunning")]
+    public float runTimer;
     public bool isWall;
     public bool wasWall;
     public Vector3 contact;
@@ -34,12 +36,15 @@ public class characterController : MonoBehaviour
     public bool isGrounded;
     public bool wasGrounded;
     public Vector3 velocity;
+    public Vector3 input;
     public float actualSpeed;
     public float actualDrag;
     public bool diagonal;
     float mouseX;
     float mouseY;
-
+    public float test;
+    public float reference0;
+    float camZ;
 
     // Start is called before the first frame update
     void Start() {
@@ -48,6 +53,18 @@ public class characterController : MonoBehaviour
         //later set objects here
     }
     void Update( ){
+        camera.transform.localEulerAngles = new Vector3(0, 0, camZ);
+
+        if (isWall) {
+            camZ = Mathf.SmoothDamp(camZ, 20, ref reference0, 0.1f);
+        }
+        else {
+            camZ = Mathf.SmoothDamp(camZ, 0, ref reference0, 0.05f);
+        }
+        //if (!isWall) camera.transform.localEulerAngles = new Vector3(0, 0, 0);
+        input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        if (isWall) runTimer += Time.deltaTime;
+        else runTimer = 0;
         if (wasWall && !isWall) wallExit();
         if (!wasWall && isWall) wallEnter();
         wasWall = isWall;
@@ -56,7 +73,7 @@ public class characterController : MonoBehaviour
         mouseX = Input.GetAxis("Mouse X");
         mouseY = Input.GetAxis("Mouse Y");
         transform.Rotate(new Vector3(0, mouseX * mouseSens));
-        camera.Rotate(new Vector3(-mouseY * mouseSens, 0f));
+        cameraParent.Rotate(new Vector3(-mouseY * mouseSens, 0f));
 
         isGrounded = groundCheck(isGrounded);
 
@@ -101,7 +118,7 @@ public class characterController : MonoBehaviour
 
     // Update is called once per frame
     void FixedUpdate() {
-        text.text = Vector3.Magnitude(new Vector3(velocity.x, 0, velocity.z)).ToString();
+        //text.text = Vector3.Magnitude(new Vector3(velocity.x, 0, velocity.z)).ToString();
 
         velocity.x = velocity.x * 1 / (actualDrag + 1);
         velocity.z = velocity.z * 1 / (actualDrag + 1);
@@ -145,13 +162,13 @@ public class characterController : MonoBehaviour
             //velocity += contact * 40;
             //velocity.y += 20;
             float reference = 0f;
-            actualGravity = Mathf.SmoothDamp(gravity / 10, gravity, ref reference, 0.005f);
-            velocity.y = Mathf.Clamp(velocity.y, -actualGravity, actualGravity);
+            actualGravity = Mathf.SmoothDamp(gravity / 10, gravity * Mathf.Pow(runTimer, 2) / (velocity.magnitude / 14 + 1), ref reference, 0.01f / velocity.magnitude);
+            velocity.y = Mathf.Clamp(velocity.y, -actualGravity * 5, 20);
             velocity += contact * -10/velocity.magnitude;
             //velocity += transform.forward;
-            if (Input.GetKeyDown(KeyCode.Space)) {
+            if (Input.GetKeyDown(KeyCode.Space) || actualGravity > gravity) { //throw player away from wall
                 velocity += Vector3.Scale(new Vector3(Mathf.Abs(velocity.x), Mathf.Abs(velocity.y), Mathf.Abs(velocity.z)), contact); 
-                velocity += contact * 15 + transform.up * 4;
+                velocity += Vector3.Scale(transform.forward, input) * 15 + transform.up * 4 + contact * 7;
 
             }
 
@@ -159,12 +176,16 @@ public class characterController : MonoBehaviour
 
     }
     void wallEnter() {
-
+        velocity += transform.up * 2;
+        print("WallEnter");
+        velocity.y = Mathf.Clamp(velocity.y, -gravity*2, gravity*2);
     }
     void wallExit() {
         actualGravity = gravity;
         velocity += Vector3.Scale(new Vector3(Mathf.Abs(velocity.x), Mathf.Abs(velocity.y), Mathf.Abs(velocity.z)) * 1.3f, contact * 1.2f);
-        print(velocity.x);
+        //print(velocity.x);
+        jumpAmount = 2;
+
 
     }
 }
